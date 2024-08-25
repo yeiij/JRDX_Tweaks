e453344cd04257c6dd054e0f0f2d14f922cc1838
Yeiij <6072745+yeiij@users.noreply.github.com>
2024-08-25 16:22:42 +0200

    fix: unlimited budget only for admin

--- scripts/Game/Editor/Components/Editor/JRDX_Tweaks/SCR_BudgetEditorComponent.c
+++ scripts/Game/Editor/Components/Editor/JRDX_Tweaks/SCR_BudgetEditorComponent.c
@@ -1,7 +1,9 @@
-modded class SCR_BudgetEditorComponent
+modded class SCR_BudgetEditorComponent : SCR_BaseEditorComponen
 {
-	override protected bool IsBudgetCapEnabled() {
-		super.IsBudgetCapEnabled();
-		return false;
+	override protected bool IsBudgetCapEnabled()
+	{
+		if (IsAdmin())
+			return false;
+		return super.IsBudgetCapEnabled();
 	}
 };
