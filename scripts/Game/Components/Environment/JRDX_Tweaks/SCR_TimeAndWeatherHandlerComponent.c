a700b616f05533d813775f4b45127be7a9a43554
Yeiij <6072745+yeiij@users.noreply.github.com>
2024-08-27 17:23:45 +0200

    feat: set day x4 night x12, default

--- scripts/Game/Components/Environment/JRDX_Tweaks/SCR_TimeAndWeatherHandlerComponent.c
+++ scripts/Game/Components/Environment/JRDX_Tweaks/SCR_TimeAndWeatherHandlerComponent.c
@@ -3,6 +3,8 @@ modded class SCR_TimeAndWeatherHandlerComponent : SCR_BaseGameModeComponent
 	override void OnPostInit(IEntity owner)
 	{
 		super.OnPostInit(owner);
+		m_fDayTimeAcceleration = 4.0;
+		m_fNightTimeAcceleration = 12.0;
 		m_bRandomWeatherChanges = true;
 	}
 };
